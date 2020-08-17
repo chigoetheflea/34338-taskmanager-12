2c43f50fffded6f8781a69f128ab31e680a3d67e
chigoetheflea <chigoetheflea@gmail.com>
2020-08-17 12:00:06 +0300

    Добавляет шаблонизацию

--- src/main.js
+++ src/main.js
@@ -6,27 +6,56 @@ import {createTasksWrapperTemplate} from "./view/tasks-wrapper.js";
 import {createCardTemplate} from "./view/card.js";
 import {createCardEditTemplate} from "./view/card-edit.js";
 import {createMoreButtonTemplate} from "./view/more-button.js";
-import {render} from "./view/render.js";
 
-const CARDS_COUNT = 3;
+import {render} from "./utils.js";
+
+import {generateTask} from "./mock/task.js";
+
+import {generateFilter} from "./mock/filter.js";
+
+const CARDS_COUNT = 20;
+const CARDS_COUNT_PER_STEP = 8;
+
+const tasks = new Array(CARDS_COUNT).fill().map(generateTask);
+const filters = generateFilter(tasks);
 
 const siteHeader = document.querySelector(`.main__control`);
 const siteMainSection = document.querySelector(`.main`);
 
 render(siteHeader, createMenuTemplate(), `beforeend`);
-render(siteMainSection, createFiltersTemplate(), `beforeend`);
+render(siteMainSection, createFiltersTemplate(filters), `beforeend`);
 render(siteMainSection, createBoardTemplate(), `beforeend`);
 
 const board = document.querySelector(`.board`);
 
 render(board, createSortingTemplate(), `beforeend`);
 render(board, createTasksWrapperTemplate(), `beforeend`);
-render(board, createMoreButtonTemplate(), `beforeend`);
 
 const cardsWrapper = document.querySelector(`.board__tasks`);
 
-render(cardsWrapper, createCardEditTemplate(), `beforeend`);
+render(cardsWrapper, createCardEditTemplate(tasks[0]), `beforeend`);
 
-for (let i = 0; i < CARDS_COUNT; i++) {
-  render(cardsWrapper, createCardTemplate(), `beforeend`);
+for (let i = 1; i < Math.min(tasks.length, CARDS_COUNT_PER_STEP); i++) {
+  render(cardsWrapper, createCardTemplate(tasks[i]), `beforeend`);
+}
+
+if (tasks.length > CARDS_COUNT_PER_STEP) {
+  let renderedTaskCount = CARDS_COUNT_PER_STEP;
+
+  render(board, createMoreButtonTemplate(), `beforeend`);
+
+  const moreButton = board.querySelector(`.load-more`);
+
+  moreButton.addEventListener(`click`, (evt) => {
+    evt.preventDefault();
+    tasks
+      .slice(renderedTaskCount, renderedTaskCount + CARDS_COUNT_PER_STEP)
+      .forEach((task) => render(cardsWrapper, createCardTemplate(task), `beforeend`));
+
+    renderedTaskCount += CARDS_COUNT_PER_STEP;
+
+    if (renderedTaskCount >= tasks.length) {
+      moreButton.remove();
+    }
+  });
 }
---
+++ src/utils.js
@@ -0,0 +1,59 @@
+const render = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+const getRandomInteger = (a = 0, b = 1) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+
+  return Math.floor(lower + Math.random() * (upper - lower + 1));
+};
+
+const getRandomBoolean = () => {
+  return Boolean(getRandomInteger(0, 1));
+};
+
+const getCurrentDate = () => {
+  const currentDate = new Date();
+  currentDate.setHours(23, 59, 59, 999);
+
+  return new Date(currentDate);
+};
+
+const isTaskExpired = (dueDate) => {
+  if (dueDate === null) {
+    return false;
+  }
+
+  const currentDate = getCurrentDate();
+
+  return currentDate.getTime() > dueDate.getTime();
+};
+
+const isTaskExpiringToday = (dueDate) => {
+  if (dueDate === null) {
+    return false;
+  }
+
+  const currentDate = getCurrentDate();
+
+  return currentDate.getTime() === dueDate.getTime();
+};
+
+const isTaskRepeating = (repeating) => {
+  return Object.values(repeating).some(Boolean);
+};
+
+const humanizeTaskDueDate = (dueDate) => {
+  return dueDate.toLocaleString(`en-US`, {day: `numeric`, month: `long`});
+};
+
+export {
+  render,
+  getRandomInteger,
+  getRandomBoolean,
+  isTaskExpired,
+  isTaskExpiringToday,
+  isTaskRepeating,
+  humanizeTaskDueDate
+};
